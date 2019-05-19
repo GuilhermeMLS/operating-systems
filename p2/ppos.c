7356634d398c7afda050e64c358a1f56b06ae4e1
GuilhermeMLS <guitaikion@gmail.com>
2019-05-19 18:48:41 -0300

    Implementa mensagens de debugger no p2

--- p2/ppos.c
+++ p2/ppos.c
@@ -9,22 +9,41 @@
 task_t task_main, *current_task;
 int task_ids;
 
-int task_create (task_t *task, void (*start_func)(void *), void *arg)
+// Constantes para mensagens de erro e debug coloridas
+#define RED "\x1B[31m"
+#define MAG "\x1B[35m"
+#define CYN "\x1B[36m"
+#define RESET "\x1B[0m"
+
+// Mensagens de erro ou debugger
+void print_message(char *color, char *func, char *message)
 {
+#ifdef DEBUG
+    printf("%s%s: %s%s\n", color, func, RESET, message);
+#endif
+}
+
+int task_create (
+    task_t *task,
+    void (*start_func)(void *),
+    void *arg
+) {
+
     getcontext (&(task->context));
     task->stack = malloc(STACKSIZE);
     if (task->stack) {
-        task->context.uc_stack.ss_sp = task->stack ;
-        task->context.uc_stack.ss_size = STACKSIZE ;
-        task->context.uc_stack.ss_flags = 0 ;
-        task->context.uc_link = 0 ;
+        task->context.uc_stack.ss_sp = task->stack;
+        task->context.uc_stack.ss_size = STACKSIZE;
+        task->context.uc_stack.ss_flags = 0;
+        task->context.uc_link = 0;
     } else {
-        perror ("Erro na criação da pilha: ") ;
+        print_message(RED, "task_create()", "Erro ao criar a pilha");
         exit (1) ;
     }
     task->id = task_ids;
     task_ids++;
     makecontext (&(task->context), (void*)(*start_func), 1, arg);
+    print_message(MAG, "task_create()", "Tarefa criada");
 
     return task->id;
 }
@@ -33,6 +52,7 @@ int task_switch (task_t *task)
 {
     task_t *t = current_task;
     current_task = task;
+    print_message(MAG, "task_switch()", "Trocando contexto");
     swapcontext(&(t->context), &(current_task->context));
 
     return task->id;
@@ -40,6 +60,7 @@ int task_switch (task_t *task)
 
 void ppos_init ()
 {
+    print_message(MAG, "ppos_init()", "Iniciando sistema operacional");
     /* desativa o buffer da saida padrao (stdout), usado pela função printf */
     setvbuf (stdout, 0, _IONBF, 0);
     task_ids = 0;
@@ -50,5 +71,6 @@ void ppos_init ()
 
 void task_exit (int exit_code)
 {
+    print_message(MAG, "task_exit()", "Saindo da tarefa");
     task_switch(&task_main);
 }
